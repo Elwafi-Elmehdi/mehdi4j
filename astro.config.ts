d2884eae1776274e56f716b663fcf51edcd5da61
Mehdi <elwafi.courrier99@gmail.com>
2023-01-20 00:03:29 +0100

    Updated Astro COnfig

--- astro.config.ts
+++ astro.config.ts
@@ -1,33 +1,41 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
-import sitemap from "@astrojs/sitemap";
-import prefetch from "@astrojs/prefetch";
+// import { defineConfig } from "astro/config";
+// import mdx from "@astrojs/mdx";
+// import tailwind from "@astrojs/tailwind";
+// import image from "@astrojs/image";
+// import sitemap from "@astrojs/sitemap";
+// import prefetch from "@astrojs/prefetch";
 
+//
+// 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.astro-theme-cactus.netlify.app",
-	experimental: {
-		contentCollections: true,
-	},
-	markdown: {
-		shikiConfig: {
-			theme: "dracula",
-			wrap: true,
-		},
-	},
-	integrations: [
-		mdx({}),
-		tailwind({
-			config: {
-				applyBaseStyles: false,
-			},
-		}),
-		image({
-			serviceEntryPoint: "@astrojs/image/sharp",
-		}),
-		sitemap(),
-		prefetch(),
-	],
+// 	site: "https://www.astro-theme-cactus.netlify.app",
+// 	experimental: {
+// 		contentCollections: true,
+// 	},
+// 	markdown: {
+// 		shikiConfig: {
+// 			theme: "dracula",
+// 			wrap: true,
+// 		},
+// 	},
+// 	integrations: [
+// 		mdx({}),
+// 		tailwind({
+// 			config: {
+// 				applyBaseStyles: false,
+// 			},
+// 		}),
+// 		image({
+// 			serviceEntryPoint: "@astrojs/image/sharp",
+// 		}),
+// 		sitemap(),
+// 		prefetch(),
+// 	],
+// });
+import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
+export default defineConfig({
+  output: 'server',
+  adapter: netlify()
 });
